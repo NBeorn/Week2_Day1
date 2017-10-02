eba537b04e9a9f0429f6dcce9b5fe36504a373bc
NBeorn <nmbeorn@gmail.com>
2017-10-02 14:13:50 -0400

    practice with 'new'

--- Week2_Day1/Program.cs
+++ Week2_Day1/Program.cs
@@ -19,20 +19,32 @@ namespace Week2_Day1
             //Console.WriteLine(studentNames[2]);
 
             //Practice from Github
-            string[] vacationSpots = { "Miami", "Cancun", "Egypt", "L.A.", "Austin", "Paris", "Rome", "Barcelona", "Japan", "Hawaii" };
-            Console.WriteLine(vacationSpots[0]);
-            Console.WriteLine(vacationSpots[9]);
+            //string[] vacationSpots = { "Miami", "Cancun", "Egypt", "L.A.", "Austin", "Paris", "Rome", "Barcelona", "Japan", "Hawaii" };
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
 
-            int[] childrenAges = { 12, 10, 7 };
-            Console.WriteLine(childrenAges[0]);
-            Console.WriteLine(childrenAges[1]);
-            Console.WriteLine(childrenAges[2]);
+            //int[] childrenAges = { 12, 10, 7 };
+            //Console.WriteLine(childrenAges[0]);
+            //Console.WriteLine(childrenAges[1]);
+            //Console.WriteLine(childrenAges[2]);
 
-            double[] studentGpa = { 4.0, 2.8, 3.5, 3.25, 2.9, 3.0, 2.0, 1.7 };
-            Console.WriteLine(studentGpa[7]);
+            //decimal[] studentGpa = { 4.0m, 2.8m, 3.5m, 3.25m, 2.9m, 3.0m, 2.0m, 1.7m };
+            //Console.WriteLine(studentGpa[0]);
 
-            string[] peersInitials = { "R", "A", "M", "D" };
-            Console.WriteLine(peersInitials[1]);
+            //string[] peersInitials = { "R", "A", "M", "D" };
+            //Console.WriteLine(peersInitials[1]);
+
+            //how to initialize when we do not know all elements from the start
+            int[] numbers = new int[4];
+            //the 4 is how many elements will exist within the new array
+            numbers[0] = 43;
+            numbers[1] = 12;
+            Console.WriteLine("Please enter a number");
+            numbers[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a number");
+            numbers[3] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(numbers[3]);
         }
     }
 }
